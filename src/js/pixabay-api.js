c266a323f7ae92d86d4d4f404615f686eaa1cd98
dvankevich <dvankevich@gmail.com>
2025-10-29 21:25:06 +0200

    remove unused functions

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -22,31 +22,3 @@ export async function getImagesAxios(searchTerm, page = 1, itemsPerPage = 15) {
     throw error;
   }
 }
-
-export function getImages(searchTerm) {
-  const options = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    },
-  };
-  const apiParams = new URLSearchParams({
-    key: getApiKey(),
-    //q: encodeURIComponent(searchTerm), // працює і без того
-    q: searchTerm,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  const queryUrl = `https://pixabay.com/api/?${apiParams}`;
-  //console.log(queryUrl);
-  return fetch(queryUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(error => console.log('Error fetching photos:', error));
-}
